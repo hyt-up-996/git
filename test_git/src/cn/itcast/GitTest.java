658c853521fb8ecf2b742d8b6f5748cb211360b2
hyt <hyt6lzl@163.com>
2021-01-26 09:27:42 +0800

    提交    2

--- test_git/src/cn/itcast/GitTest.java
+++ test_git/src/cn/itcast/GitTest.java
@@ -8,5 +8,6 @@ package cn.itcast;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("提交    1    ");
+        System.out.println("提交    2    ");
     }
 }
